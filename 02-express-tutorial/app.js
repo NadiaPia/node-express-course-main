9e086d2ef0cf618370afe6604a5bf90b89b3c7e2
NadiaPia <206i417@gmail.com>
2023-01-06 11:36:46 -0800

    add middleware function and practice app.use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,77 +1,25 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data')
+const logger  = require('./logger');
 
-//server responses with the json data
-/*
-app.get('/', (req,res)=> {
-  res.json(products)
-})*/
+app.use(logger)
+//app.use('/api', logger) //if to specify the url the middleware function logger will work for the routes that go after '/api'
 
-//server responses with the html
-app.get('/', (req,res)=> {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+
+app.get('/', (req,res)=>{
+  res.send("Home")
 });
 
-app.get('/api/products', (req,res)=> {
-  const newProducts = products.map((product) => {
-    const {id, name, image} = product
-    return {id, name, image}
-  })
-  res.json(newProducts)
-
+app.get('/about', (req,res)=>{
+  res.send("About")
 })
 
-app.get('/api/products', (req,res)=> {
-  res.json(products.map((item) => {
-    return [item.id, item.name, item.image, item.prise ]
-  }))
-
+app.get('/api/products', (req,res)=>{
+  res.send("products")
 })
 
-app.get('/api/products/:productID', (req,res)=> {
-  //console.log('reqreqreqreqreq', req)  
-  //console.log('reqreqreqreqreq', req.params)
-  //const productID =  req.params.productID //the same as const {productID} = req.params
-
-  const {productID} = req.params //the same as const productID =  req.params.productID, because req.params is an object  
-  const singleProduct = products.find((product) => product.id === Number(productID))
-  
-  if(!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
-  return res.json(singleProduct)
-
-})
-
-app.get('/api/products/:productID/reviews/:review', (req, res)=> {
-  res.send("hello World")
-
-})
-
-
-
-
-app.get('/api/v1/query', (req,res)=>{
-  //console.log("req.queryreq.queryreq.queryreq.query", req.query);
-  const search = req.query.search;
-  //const{ search, limit } = req.query
-  const limit = req.query.limit;
-  let sortedProducts = [...products];
-  if(search) {
-    sortedProducts=sortedProducts.filter((product)=>{
-      return product.name.startsWith(search)
-    })
-  }
-  if(limit){
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-
-  if(sortedProducts.length < 1) {
-    return res.status(200).send('No Product Match Your Search')
-  }
- res.status(200).json(sortedProducts)
-
+app.get('/api/items', (req,res)=>{
+  res.send("items")
 })
 
 
