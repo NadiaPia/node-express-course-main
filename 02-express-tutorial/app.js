346950ff1b6b647f0312250508eb56d4498cc78b
NadiaPia <206i417@gmail.com>
2023-01-09 10:48:08 -0800

    Our own middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,13 @@
+
 const express = require('express');
 const app = express();
 const logger  = require('./logger');
 const authorize  = require('./authorize');
 
+// middleware functions have options: 1. our own - we can set up our own middleware, for example authorize or login
+//2. express - it provides a few build in middleware functions. We don't worry about the functionality, 
+//but just reference the docs: ex: app.use(express.static('./public'));
+//3. third party - we have to install it.
 
 app.use([authorize, logger])
 //app.use('/api', logger) //if to specify the url the middleware function logger will work for the routes that go after '/api'
