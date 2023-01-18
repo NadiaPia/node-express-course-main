6ed5d54dd0de087fdfd3cb3f83939aab83f72229
NadiaPia <206i417@gmail.com>
2023-01-17 16:55:25 -0800

    add try catch block to the validation in cteateTask block

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,8 +5,12 @@ const getAllTasks = (req,res) => {
 }
 
 const createTask = async (req,res) => {
-    const task = await Task.create(req.body) //create a 'task' collection in mongoDB
-    res.status(201).json({ task })
+    try {
+        const task = await Task.create(req.body) //create a 'task' collection in mongoDB
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
 const getTask = (req,res) => {
