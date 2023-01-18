29f885df5af8795f8ebcaae00fce92e0d986428a
NadiaPia <206i417@gmail.com>
2023-01-17 16:24:34 -0800

    Add validation method

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed:Boolean,
+//for the validation we should use value for the keys as the objects. 
+//For ex. not "name": Srting, but "name": {}.
+//It allows set up more restrictions for the inserting data.
+//required: [true, 'must provide name'] doesn't allow to send an empty data. Second value of the array is the custom message
+//trim - skips empty symbols (white space) ("   nadia  " = "nadia")
+
+
+const TaskSchema = new mongoose.Schema({   
+    name: {                                    
+        type: String,                         
+        required: [true, 'must provide name'],    
+        trim: true,                               
+        maxlength: [20, 'name can not be more than 20 characters']
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    }
 });
 
 
